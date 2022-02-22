b267bf864cbec6e2d7f47a20fc020f54bfa03798
pschankin06 <schankin2089@gmail.com>
2022-02-22 10:22:06 +0300

    deleted comments

--- js/view.js
+++ js/view.js
@@ -1,24 +1,3 @@
-// export const uiElements = {
-//     'display': document.querySelector('.calc__display'),
-//     'resetButton': document.querySelector('.btn-clear'),
-//     'backspaceButton': document.querySelector('.btn-erase'),
-//     'digits': document.querySelector('.btn-num'),
-//     'operators': document.querySelector('.btn-oper'),
-//     'equals': document.querySelector('.btn-equals')
-// }
-
-
-
-
-// const uiElements = {
-//     'display': document.querySelector('.calc__display'),
-//     'resetButton': document.querySelector('.btn-clear'),
-//     'backspaceButton': document.querySelector('.btn-erase'),
-//     'digits': document.querySelector('.btn-num'),
-//     'operators': document.querySelector('.btn-oper'),
-//     'equals': document.querySelector('.btn-equals'),
-// }
-
 export const display = document.querySelector('.calc__display');
 export const resetButton = document.querySelector('.btn-clear');
 export const backspaceButton = document.querySelector('.btn-erase');
